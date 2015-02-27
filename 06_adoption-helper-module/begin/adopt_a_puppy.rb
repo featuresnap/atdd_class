919c3bcbbca0f1d5ee1e92b701f75b46f3057d67
lab <lab@leandog.com>
2015-02-27 06:31:27 -0500

    Updated single puppy begin state to account for previous refactorings of extracting method and variable

--- 06_adoption-helper-module/begin/adopt_a_puppy.rb
+++ 06_adoption-helper-module/begin/adopt_a_puppy.rb
@@ -1,7 +1,12 @@
 require 'rubygems'
 require 'watir-webdriver'
-browser=Watir::Browser.new
-browser.goto 'http://puppies.herokuapp.com'
-browser.button(:value => 'View Details').click
-browser.button(:value => 'Adopt Me!').click
-browser.button(:value => 'Complete the Adoption').click
+
+def adopt_puppy_number(puppy_number)
+  @browser.button(:value => 'View Details', :index => puppy_number - 1).click
+  @browser.button(:value => 'Adopt Me!').click
+end
+
+@browser=Watir::Browser.new
+@browser.goto 'http://puppies.herokuapp.com'
+adopt_puppy_number 1
+@browser.button(:value => 'Complete the Adoption').click
