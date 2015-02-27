0ab7903097f6a8220e389f7709011504756ffa6b
lab <lab@leandog.com>
2015-02-26 21:13:53 -0500

    added complete the adoption step

--- 03_adopt-a-puppy/end/adopt-a-puppy.rb
+++ 03_adopt-a-puppy/end/adopt-a-puppy.rb
@@ -4,3 +4,4 @@ browser=Watir::Browser.new
 browser.goto 'http://puppies.herokuapp.com'
 browser.button(:value => 'View Details').click
 browser.button(:value => 'Adopt Me!').click
+browser.button(:value => 'Complete the Adoption').click
