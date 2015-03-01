33bcedd29307e010c334fa13b7ca935b7a92928d
lab <lab@leandog.com>
2015-03-01 18:04:21 -0500

    added order completion and verification to steps 1 & 2 of exercise 5

--- 05_dry-it-up/01-begin/adopt_two_puppies.rb
+++ 05_dry-it-up/01-begin/adopt_two_puppies.rb
@@ -2,9 +2,21 @@ require 'rubygems'
 require 'watir-webdriver'
 browser=Watir::Browser.new
 browser.goto 'http://puppies.herokuapp.com'
+
 browser.button(:value => 'View Details', :index => 0).click
 browser.button(:value => 'Adopt Me!').click
+
 browser.button(:value => 'Adopt Another Puppy').click
+
 browser.button(:value => 'View Details', :index => 1).click
 browser.button(:value => 'Adopt Me!').click
+
 browser.button(:value => 'Complete the Adoption').click
+
+browser.text_field(:id => 'order_name').set 'Jim Bob'
+browser.text_field(:id => 'order_address').set "123 Main Street\nCleveland, OH 44113" 
+browser.text_field(:id => 'order_email').set 'jimbob@atdd.com'
+browser.select_list(:id => 'order_pay_type').select 'Check'
+browser.button(:value => 'Place Order').click
+
+fail 'Something went wrong adopting a puppy' unless browser.text.include? 'Thank you for adopting a puppy!' 
--- 05_dry-it-up/02-method/adopt_two_puppies.rb
+++ 05_dry-it-up/02-method/adopt_two_puppies.rb
@@ -13,3 +13,11 @@ adopt_puppy_number(1, browser)
 browser.button(:value => 'Adopt Another Puppy').click
 adopt_puppy_number(2, browser)
 browser.button(:value => 'Complete the Adoption').click
+
+browser.text_field(:id => 'order_name').set 'Jim Bob'
+browser.text_field(:id => 'order_address').set "123 Main Street\nCleveland, OH 44113" 
+browser.text_field(:id => 'order_email').set 'jimbob@atdd.com'
+browser.select_list(:id => 'order_pay_type').select 'Check'
+browser.button(:value => 'Place Order').click
+
+fail 'Something went wrong adopting a puppy' unless browser.text.include? 'Thank you for adopting a puppy!' 
