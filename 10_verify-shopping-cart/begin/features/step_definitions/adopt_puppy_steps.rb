6b649cfe6f752fb08e9e672d996157dc82c2fbbf
lab <lab@lab.com>
2015-04-13 19:42:15 -0400

    removed steps not needed for exercise

--- 10_verify-shopping-cart/begin/features/step_definitions/adopt_puppy_steps.rb
+++ 10_verify-shopping-cart/begin/features/step_definitions/adopt_puppy_steps.rb
@@ -8,33 +8,8 @@ And(/^I adopt puppy (\d+)$/) do |puppy_number|
   @browser.button(:value => 'Adopt Me!').click
 end
 
-And(/^I complete the adoption$/) do
-  @browser.button(:value => 'Complete the Adoption').click
-end
-
 And(/^I adopt another puppy$/) do
   @browser.button(:value => 'Adopt Another Puppy').click
-end
-
-And(/^I enter the name "([^"]*)"$/) do |name|
-  @browser.text_field(:id => 'order_name').value = name
-end
-
-And(/^I enter the address "([^"]*)"$/) do |address|
-  puts address
-  @browser.textarea(:id => 'order_address').value = address
-end
-
-And(/^I enter the contact email "([^"]*)"$/) do |email|
-  @browser.text_field(:id => 'order_email').value = email
-end
-
-And(/^I enter "([^"]*)" as the payment type$/) do |payment_method|
-  @browser.select(:id => 'order_pay_type').select(payment_method)
-end
-
-When(/^I place my order$/) do
-  @browser.button(:value => 'Place Order').click
 end
 
 Then(/^I should see the message "([^"]*)"$/) do |expected_message|
