a4413466e3ac9a64190d62428af3d08678221992
Randy Eppinger <Randy.Eppinger@leandog.com>
2015-04-15 21:46:06 -0400

    Removed error div elements which are unused in the scenarios for datamagic.

--- 20_datamagic/features/support/pages/order_page.rb
+++ 20_datamagic/features/support/pages/order_page.rb
@@ -8,10 +8,6 @@ class OrderPage
   text_field(:name, :id => 'order_name')
   select(:pay_type, :id => 'order_pay_type')
   button(:place_order, :value => 'Place Order')
-  div(:error_list, id: 'error_explanation')
-  unordered_list(:errors) { |page|
-      page.error_list_element.unordered_list_element
-  }
 
   def checkout(order = {})
     populate_page_with data_for(:checkout_page, order)
